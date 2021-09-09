b4e1d90cca89569f767adcda3e976c9be645ed17
mavro <mrojasb2000@gmail.com>
2021-09-08 21:23:46 -0300

    Refactor appointments day view test

--- test/Appointments.test.js
+++ test/Appointments.test.js
@@ -27,6 +27,11 @@ describe('Appointments', () => {
 
 describe('AppointmentsDayView', () => {
     let container;
+    const today = new Date();
+    const appointments = [
+        { startAt: today.setHours(12, 0) },
+        { startAt: today.setHours(13, 0) }
+    ];
 
     beforeEach(() => {
         container = document.createElement('div');
@@ -40,22 +45,12 @@ describe('AppointmentsDayView', () => {
     });
 
     it('renders multiple appointments in an ol element', () => {
-        const today = new Date();
-        const appointments = [
-            { startAt: today.setHours(12, 0) },
-            { startAt: today.setHours(13, 0) }
-        ];
         render(<AppointmentsDayView appointments={appointments} />);
         expect(container.querySelector('ol')).not.toBeNull();
         expect(container.querySelector('ol').children).toHaveLength(2);
     })
 
     it('renders each appointment in an li', () => {
-        const today = new Date();
-        const appointments = [
-            { startAt: today.setHours(12, 0) },
-            { startAt: today.setHours(13, 0) }
-        ];
         render(<AppointmentsDayView appointments={appointments} />)
         expect(container.querySelectorAll('li')).toHaveLength(2);
         expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
